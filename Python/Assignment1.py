c4c5a793f5f7e912b60d12bcc2d839a3104ad722
Jemima Ekanem <jemimaekanem6@gmail.com>
2026-04-30 18:18:18 +0100

    first commit

---
+++ Python/Assignment1.py
@@ -0,0 +1,6 @@
+#Area of a square
+side = int(input("Enter the side of the square: "))
+
+area = side * side
+
+print(f"Area of the square is: {area}")
---
+++ Python/Assignment2.py
@@ -0,0 +1,34 @@
+#Shopping cart
+item1 = input("Enter name of item: ")
+qty1 = int(input("Enter quantity: "))
+price1 = float(input("Enter price: "))
+total1 = qty1 * price1
+print(total1)
+
+item2 = input("Enter name of item: ")
+qty2 = int(input("Enter quantity: "))
+price2 = float(input("Enter price: "))
+total2 = qty2 * price2
+print(total2)
+
+item3 = input("Enter name of item: ")
+qty3 = int(input("Enter quantity: "))
+price3 = float(input("Enter price: "))
+total3 = qty3 * price3
+print(total3)
+
+item4 = input("Enter name of item: ")
+qty4 = int(input("Enter quantity: "))
+price4 = float(input("Enter price: "))
+total4 = qty4 * price4
+print(total4)
+
+item5 = input("Enter name of item: ")
+qty5 = int(input("Enter quantity: "))
+price5 = float(input("Enter price: "))
+total5 = qty5 * price5
+print(total5)
+
+total_price = total1 + total2 + total3 + total4 + total5
+
+print("Total price:", total_price)
